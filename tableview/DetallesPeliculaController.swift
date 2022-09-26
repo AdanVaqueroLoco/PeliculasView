77e2a0725a3c5dfd3c529394f32310b90129adfd
Alumno <ulsa@D109.local>
2022-09-26 08:49:44 -0700

    first commit

---
+++ tableview/DetallesPeliculaController.swift
@@ -0,0 +1,24 @@
+//
+//  DetallesPeliculaController.swift
+//  tableview
+//
+//  Created by Alumno on 26/09/22.
+//  Copyright © 2022 MaestroD1. All rights reserved.
+//
+
+import UIKit
+
+class DetallesPeliculaController : UIViewController {
+    
+    var pelicula : Pelicula?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Detalles de película"
+        
+        if pelicula != nil {
+            self.title = pelicula?.titulo
+        }
+    }
+}
